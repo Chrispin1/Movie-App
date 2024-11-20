e77acd441042fa814ad4af10566d261b2b7eb4c2
Chrispin <kiokondolo1@gmail.com>
2024-11-20 23:37:45 +0300

    fetched data from the API and made the data available to the card component via props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,29 +1,40 @@
 import godfather from "../assets/img.png";
 
-const Card = () => {
+const Card = ({ cardWidth, movie }) => {
+  const { title, image, genres, originalLanguage, releaseDate, overview } =
+    movie;
   return (
-    <div className="h-[650px] relative flex justify-center items-center shrink-0 p-2 bg-[#403D39] group">
-      <div className="w-[97%] h-[97%] absolute m-auto text-white rounded-lg flex flex-col justify-center bg-black/50 p-10 gap-y-2 cursor-pointer">
-        <h1 className="text-4xl">The GodFather</h1>
+    <div
+      style={{ width: cardWidth }}
+      className="h-[650px] relative flex justify-center items-center shrink-0 p-2 bg-[#403D39] group"
+    >
+      <div className="w-[97%] h-[97%] absolute m-auto text-white rounded-lg flex flex-col justify-center bg-black/50 p-10 gap-y-2 cursor-pointer group z-10 group-hover:hidden transition-all">
+        <h1 className="text-4xl">{title}</h1>
         <div className="flex items-center gap-x-2 ">
           <span className="text-lg">Genres: </span>
-          <span className="font-semibold text-red-500">Crime Drama</span>
+          {genres.map((genre, index) => (
+            <span key={index} className="font-semibold text-red-500">
+              {genre}
+            </span>
+          ))}
         </div>
         <span className="gap-x-2 flex">
-          Original Language: <span className="mr-2 uppercase">EN</span>
+          Original Language:{" "}
+          <span className="mr-2 uppercase">{originalLanguage}</span>
         </span>
         <span className="flex gap-x-2">
-          Release Date: <span className="mr-2 text-yellow-400">1972-03-04</span>
+          Release Date:{" "}
+          <span className="mr-2 text-yellow-400">{releaseDate}</span>
         </span>
         <p className="flex flex-col gap-y-2">
           <span className="text-red-500">Summary: </span>
-          <span className=" first-letter:pl-2">Summary Text</span>
+          <span className=" first-letter:pl-2">{overview}</span>
         </p>
       </div>
       <img
-        src={godfather}
+        src={image}
         alt="Movie Image"
-        className="rounded-xl object-cover opacity-50 group-hover:opacity-100 transition-opacity duration-500"
+        className=" absolute w-[97%] h-[97%] rounded-xl opacity-50 group-hover:opacity-100 object-cover transition-opacity duration-500 z-0"
       />
     </div>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Card from "./Card";
 
 const Home = () => {
   const [cardWidth, setCardWidth] = useState(500);
   const cardInRow = 5;
   const [wrapperWidth, setWrapperWidth] = useState(cardWidth * cardInRow);
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    const getMovies = async () => {
+      const url =
+        "https://tvshow.p.rapidapi.com/Movie/NowPlaying?Page=1&Language=en-US&Adult=true";
+      const options = {
+        method: "GET",
+        headers: {
+          "x-rapidapi-key":
+            "368bb91a40msh5a0dbb05e4412edp19e4c2jsn07f1ede22403",
+          "x-rapidapi-host": "tvshow.p.rapidapi.com",
+        },
+      };
+
+      try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        setMovies(result);
+        console.log(result);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getMovies();
+  }, []);
   return (
     <div
       className="flex justify-center items-center "
       style={{ width: wrapperWidth }}
     >
       <div className="flex flex-wrap">
-        <div>
-          <Card />
-        </div>
+        {movies.map((movie, index) => (
+          <div key={index}>
+            <Card movie={movie} cardWidth={cardWidth} />
+          </div>
+        ))}
       </div>
     </div>
   );
